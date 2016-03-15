d9423168e7a8e018521b9fae8ce5e75cbd570e20
Karl Tarbe <karulont@gmail.com>
2016-03-15 19:17:53 +0200

    Move unimportant things away

---
+++ gur/consts.py
@@ -0,0 +1,4 @@
+xsize = 2
+ysize = 2
+maxt = 15
+K = 0
--- gur/model.py
+++ gur/model.py
@@ -1,92 +1,10 @@
 from gurobipy import *
 import itertools
 import sys
-
-def makeWhat(K):
-    what = set()
-    what.add('e')
-    what.add('c')
-    scj = set()
-    for j in range(K):
-        scj.add('sc'+str(j))
-    what.add('r')
-    what.add('rc')
-    rscj = set()
-    for j in range(K):
-        rscj.add('rsc'+str(j))
-    what.add('cr')
-    scrj = set()
-    for j in range(K):
-        scrj.add('scr'+str(j))
-    what.add('lft')
-    slftj = set()
-    for j in range(K):
-        slftj.add('slft'+str(j))
-    what.add('drp')
-    sdrpj = set()
-    for j in range(K):
-        sdrpj.add('sdrp'+str(j))
-
-    what = what.union(scj,rscj,scrj,slftj,sdrpj)
-    return what, scj, rscj, scrj, slftj, sdrpj
+from stuff import *
+from consts import *
 
 def createModel():
-    xsize = 2
-    ysize = 2
-    maxt = 10
-
-    timeiter = range(maxt)
-    diriter = range(4)
-
-    def nodes():
-        return itertools.product(range(xsize),range(ysize))
-
-    def neighbours(node):
-        x,y = node
-        if x > 0:
-            yield (x-1,y)
-        if y > 0:
-            yield (x,y-1)
-        if x < xsize - 1:
-            yield (x+1,y)
-        if y < ysize - 1:
-            yield (x,y+1)
-
-    WEST = 0
-    NORTH = 1
-    EAST = 2
-    SOUTH = 3
-
-    def dir2c(d):
-        if d == WEST:
-            return 'W'
-        elif d == NORTH:
-            return 'N'
-        elif d == EAST:
-            return 'E'
-        elif d == SOUTH:
-            return 'S'
-
-    def oppositeDir(d):
-        return (d+2) % 4
-
-    def edg(node, d):
-        assert d >= 0 and d <= 3
-        x,y = node
-        if d == WEST: # west
-            return (x - 1, y)
-        elif d == NORTH: # north
-            return (x, y - 1)
-        elif d == EAST:
-            return (x+1, y)
-        elif d == SOUTH:
-            return (x, y + 1)
-
-    K = 0
-    what, scj, rscj, scrj, slftj, sdrpj = makeWhat(K)
-    moveWhat = set(['r', 'cr', 'rc']).union(rscj, scrj)
-    liftWhat = set(['rc']).union(rscj)
-    dropWhat = set(['cr']).union(scrj)
 
     nstat = {}
     occu = {}
